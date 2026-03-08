338e0310a54c7a35fea04a70a78c2a1a1900190a
Meatchop <carlakira@gmail.com>
2026-03-08 19:50:43 +0800

    Update Stack.cpp

--- Stack.cpp
+++ Stack.cpp
@@ -3,15 +3,18 @@ template <typename T>
 Stack<T>::Stack(size_t c): capacity(c), size(0), head(nullptr)
 {
 };
+template <typename T>
 struct Node<T>* Stack<T>::peek(){
     return this->head;
 };
+template <typename T>
 void Stack<T>::pop(){
     if(this->size == 0) return;
     size--;
     delete this->head;
     this->head = peek()->next;
 };
+template <typename T>
 void Stack<T>::push(struct Node<T> &t)
 {
     if(this.capacity==this->size)
